e38f6a4f5e54d460cf5b30059fd26dae47f21f02
lowPrio12 <ninar.ranin@gmail.com>
2024-09-24 14:33:41 +0800

    added codes in multi dem arrays

--- src/javaproject/MultiDimensionalArray.java
+++ src/javaproject/MultiDimensionalArray.java
@@ -3,7 +3,17 @@ package multidimen;
 public class MultiDimen {
 
     public static void main(String[] args) {
-        
+        String[][] memNames = {
+            {"jacob", "mark", "kent", "joana"},
+            {"pauline", "katel", "kristine", "dexter"},};
+
+        for (int i = 0; i < memNames.length; i++) {
+            for (int j = 0; j < memNames[i].length; j++) {
+                System.out.println(memNames[i][j] + "");
+            }
+            System.out.println();
+
+        }
 
     }
 }
