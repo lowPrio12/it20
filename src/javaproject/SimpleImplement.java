001749de0afdda876855e1ba5b185d952bb74288
lowPrio12 <ninar.ranin@gmail.com>
2024-09-24 14:15:02 +0800

    added SimpleImplementation

---
+++ src/javaproject/SimpleImplement.java
@@ -0,0 +1,15 @@
+package simpleimplement;
+
+public class SimpleImplement {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {4, 1, 2, 3, 5};
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+    }
+}
