e7e74016b003ed3b251dbeba242180d61d7f4c8f
lowPrio12 <ninar.ranin@gmail.com>
2024-09-24 14:23:05 +0800

    added for loop array code

--- src/javaproject/ViaLooping.java
+++ src/javaproject/ViaLooping.java
@@ -3,7 +3,14 @@ package vialooping;
 public class ViaLooping {
 
     public static void main(String[] args) {
-        
+        int[] numbers = {4, 1, 2, 3, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            
+            System.out.println(numbers[i]);
+            
+        }
+
     }
 
 }
