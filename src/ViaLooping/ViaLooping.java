782786dfa63ce038e579827a54a353f1ecc23798
lowPrio12 <ninar.ranin@gmail.com>
2024-09-27 13:13:02 +0800

    added via looping package

--- src/ViaLooping/ViaLooping.java
+++ src/ViaLooping/ViaLooping.java
@@ -4,7 +4,13 @@ public class ViaLooping {
 
     public static void main(String[] args) {
 
-       
+        int[] numbers = {4, 1, 2, 3, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.println(numbers[i]);
+
+        }
 
     }
 }
