860719b78024ee5438bf9d2d57ea693ed181ba3a
lowPrio12 <ninar.ranin@gmail.com>
2024-09-27 13:14:34 +0800

    added multi Dimensional array

--- src/MultiDimensional/MultiDimensionalArray.java
+++ src/MultiDimensional/MultiDimensionalArray.java
@@ -3,7 +3,16 @@ package MultiDimensional;
 public class MultiDimensionalArray {
 
     public static void main(String[] args) {
-        
+        String[][] memNames = {
+            {"jacob", "mark", "kent", "joana"},
+            {"pauline", "katel", "kristine", "dexter"},};
 
+        for (int i = 0; i < memNames.length; i++) {
+            for (int j = 0; j < memNames[i].length; j++) {
+                System.out.println(memNames[i][j] + "");
+            }
+            System.out.println();
+
+        }
     }
 }
