a5b712188ef67562095a126f93f7819b9810db3e
lowPrio12 <ninar.ranin@gmail.com>
2024-10-01 15:56:43 +0800

    added scanner and elements to the list in main class, added constructor in node class and added method to add new node at the end of the list

--- src/topic_2_linkedlist_int/LinkedList.java
+++ src/topic_2_linkedlist_int/LinkedList.java
@@ -2,6 +2,31 @@ package topic_2_linkedlist_int;
 
 
 public class LinkedList {
-   
+    private Node head; 
+
+    // Method to add a new node at the end of the list
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    
+    // Method to print the linked list
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    
     
 }
--- src/topic_2_linkedlist_int/main.java
+++ src/topic_2_linkedlist_int/main.java
@@ -1,8 +1,30 @@
 package topic_2_linkedlist_int;
 
+import java.util.Scanner;
+
 public class main {
-
     public static void main(String[] args) {
+        Scanner wow = new Scanner(System.in);
+		
+		LinkedList list = new LinkedList();
 
-    }
+        // Adding elements to the list
+        System.out.println("Enter the list of elements: ");
+        int number1 = wow.nextInt();
+        int number2 = wow.nextInt();
+        int number3 = wow.nextInt();
+        int number4 = wow.nextInt();
+        int number5 = wow.nextInt();
+              
+        list.add(number1);
+        list.add(number2);
+        list.add(number3);
+        list.add(number4);
+        list.add(number5);     
+        
+        System.out.println("Current Linked List: ");
+        list.printList();
+        
+       
+}
 }
