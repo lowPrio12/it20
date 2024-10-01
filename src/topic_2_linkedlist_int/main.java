cc52e75ee1e3c6bd4566bb26bf6a36af196d9afb
lowPrio12 <ninar.ranin@gmail.com>
2024-10-01 15:59:29 +0800

    added delete elements in the main class, and method to delete node by value in linkedlist class

--- src/topic_2_linkedlist_int/main.java
+++ src/topic_2_linkedlist_int/main.java
@@ -3,10 +3,11 @@ package topic_2_linkedlist_int;
 import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args) {
         Scanner wow = new Scanner(System.in);
-		
-		LinkedList list = new LinkedList();
+
+        LinkedList list = new LinkedList();
 
         // Adding elements to the list
         System.out.println("Enter the list of elements: ");
@@ -15,16 +16,24 @@ public class main {
         int number3 = wow.nextInt();
         int number4 = wow.nextInt();
         int number5 = wow.nextInt();
-              
+
         list.add(number1);
         list.add(number2);
         list.add(number3);
         list.add(number4);
-        list.add(number5);     
-        
+        list.add(number5);
+
         System.out.println("Current Linked List: ");
         list.printList();
-        
-       
-}
+
+        //Delete  the elements
+        System.out.println("Enter the number you want to delete: ");
+        System.out.println();
+        System.out.println("Deleting a number/numbers from the list...");
+        System.out.println("Current Linked List:");
+        int number6 = wow.nextInt();
+        list.deleteByValue(number6);
+        list.printList();
+
+    }
 }
