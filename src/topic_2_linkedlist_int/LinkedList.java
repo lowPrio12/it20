98dd6a4a9f039b176be5f24f34503b5f15aabd0a
saldivar <evelinesaldivar@gmail.com>
2024-10-01 15:34:05 +0800

    added scanner and elements to the list in main class, added constructor in node class and added method to add new node at the end of the list

--- src/topic_2_linkedlist_int/LinkedList.java
+++ src/topic_2_linkedlist_int/LinkedList.java
@@ -2,8 +2,20 @@ package topic_2_linkedlist_int;
 
 
 public class LinkedList {
-    
-   
+    private Node head; 
+
+    // Method to add a new node at the end of the list
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
     }
     
-
+}
--- src/topic_2_linkedlist_int/main.java
+++ src/topic_2_linkedlist_int/main.java
@@ -1,8 +1,21 @@
 package topic_2_linkedlist_int;
 
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        Scanner wow = new Scanner(System.in);
+		
+		LinkedList list = new LinkedList();
+
+        // Adding elements to the list
+        System.out.println("Enter the list of elements: ");
+        int number1 = wow.nextInt();
+        int number2 = wow.nextInt();
+        int number3 = wow.nextInt();
+        int number4 = wow.nextInt();
+        int number5 = wow.nextInt();
+              
         
 	}
 }
