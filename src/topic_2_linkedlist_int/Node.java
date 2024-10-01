6a6e3e121a37f4f4118713493d7a208a168e2617
lowPrio12 <ninar.ranin@gmail.com>
2024-10-01 15:53:41 +0800

    added a packaged named topic_2_linkedlist_int, and added the following classes; main, node and linkedlist

--- src/topic_2_linkedlist_int/Node.java
+++ src/topic_2_linkedlist_int/Node.java
@@ -1,11 +1,5 @@
 package topic_2_linkedlist_int;
 public class Node {
-    int data;  
-    Node next;  
-
-    // Constructor
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
+    
+    
 }
