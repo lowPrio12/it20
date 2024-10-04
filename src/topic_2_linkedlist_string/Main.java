13bf3bfa74fe61db4a46627520c24446c7117a38
lowPrio12 <ninar.ranin@gmail.com>
2024-10-04 12:53:59 +0800

    added scanner and static variables and block og methods at main class, added node head at linkedlist class and method to add new node, and added variable and constructor in node class

--- src/topic_2_linkedlist_string/Main.java
+++ src/topic_2_linkedlist_string/Main.java
@@ -1,15 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topic_2_linkedlist_string;
 
-/**
- *
- * @author HP
- */
+import java.util.Scanner;
+
 public class Main {
-    
+
+    //static variables so it can be accessible within this class.
+    public static LinkedList list = new LinkedList();
+    public static Scanner sc = new Scanner(System.in);
+
+    //This is a block of methods
+    public static void adding() {
+        boolean repeat = true;
+
+        while (repeat) {
+            System.out.println("What element would you like to add?");
+            System.out.print("Element: ");
+            String elemt = sc.next();
+            list.add(elemt);
+            System.out.println("Element ' " + elemt + " ' added successfully");
+            System.out.println("Would you like to add more?" + "\nPress 1 for YES \nPress 2 for NO");
+            int resp = sc.nextInt();
+
+            if (resp == 1) {
+                repeat = true;
+            } else if (resp == 2) {
+                repeat = false;
+            } else {
+                System.out.println("Invalid input. Please enter 1 or 2.");
+                sc.close();
+                break;
+            }
+
+        }
+
+    }
+
 }
