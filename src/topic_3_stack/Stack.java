13fcfb7dd04f39af169a20c6ec5c0fa651b667d1
lowPrio12 <ninar.ranin@gmail.com>
2024-10-11 18:01:52 +0800

    Add maxSize, stackArray, and top instance variables

--- src/topic_3_stack/Stack.java
+++ src/topic_3_stack/Stack.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topic_3_stack;
 
-/**
- *
- * @author Ranin
- */
 public class Stack {
-    
+
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
 }
