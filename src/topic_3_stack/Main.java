dd31b4ee417a343ce321eea6494aa45d986caa28
lowPrio12 <ninar.ranin@gmail.com>
2024-10-11 18:20:15 +0800

    Initialize Stack with size 5, add print greeting messaage and add a while loop and add a display menu options

--- src/topic_3_stack/Main.java
+++ src/topic_3_stack/Main.java
@@ -6,5 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Stack stack = new Stack(5);
+        System.out.println("Hello there!");
+        
+        while (true) {
+            System.out.println("Choose what you want to do: "
+                    + " 1. Size"
+                    + " 2. Push"
+                    + " 3. Pop"
+                    + " 4. Exit");
+            int choice = sc.nextInt();
+        }
+
     }
 }
