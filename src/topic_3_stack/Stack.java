7a825b7eaa3ee5a0d9c510d8b0e067828570761d
lowPrio12 <ninar.ranin@gmail.com>
2024-10-11 18:03:30 +0800

    Add constructor for Stack class

--- src/topic_3_stack/Stack.java
+++ src/topic_3_stack/Stack.java
@@ -5,4 +5,10 @@ public class Stack {
     private int maxSize;
     private int[] stackArray;
     private int top;
+
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+    }
 }
