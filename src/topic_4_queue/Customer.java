7d4e61ed75b60415d698df459dfe48bb94130d3c
lowPrio12 <ninar.ranin@gmail.com>
2024-10-15 19:46:04 +0800

    Add Customer class for queue management
    Created a Customer class to represent customers in the queue system. This class includes a constructor for initializing the customer's name and a getter method for retrieving it. This is a foundational component for the queue management functionality.

---
+++ src/topic_4_queue/Customer.java
@@ -0,0 +1,13 @@
+package topic_4_queue;
+
+public class Customer {
+	private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
