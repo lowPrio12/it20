9e0edac67533a8eff4c95e3bfb61dd3fd4afe3af
lowPrio12 <ninar.ranin@gmail.com>
2024-10-15 20:07:32 +0800

    Added a method that displays the current state of the queue

--- src/topic_4_queue/Queue.java
+++ src/topic_4_queue/Queue.java
@@ -19,4 +19,16 @@ public class Queue {
         customers.addLast(customer);
         System.out.println(customer.getName() + " has been added to the queue.");
     }
+    public void displayQueue() {
+        if (isEmpty()) {
+            System.out.println("The queue is currently empty.");
+        } else {
+            System.out.print("Current queue: ");
+            for (Customer customer : customers) {
+                System.out.print(customer.getName() + " | ");
+            }
+            System.out.println();
+        }
+    }
+    
 }
