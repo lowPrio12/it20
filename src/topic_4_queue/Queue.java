5860b0f297180f0b8da713be938691a33c10f603
lowPrio12 <ninar.ranin@gmail.com>
2024-10-15 19:52:39 +0800

    Add constructor to Queue class to initialize customer list

--- src/topic_4_queue/Queue.java
+++ src/topic_4_queue/Queue.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topic_4_queue;
+import java.util.LinkedList;
 
-/**
- *
- * @author Ranin
- */
 public class Queue {
-    
+	private LinkedList<Customer> customers;
+
+    public Queue() {
+        customers = new LinkedList<>();
+    }
 }
