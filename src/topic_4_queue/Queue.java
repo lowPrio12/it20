e31426140780722eb3eb52e96ffdc0886587a39c
lowPrio12 <ninar.ranin@gmail.com>
2024-10-15 19:54:51 +0800

    Implement methods to check queue status and enqueue customers

--- src/topic_4_queue/Queue.java
+++ src/topic_4_queue/Queue.java
@@ -7,4 +7,16 @@ public class Queue {
     public Queue() {
         customers = new LinkedList<>();
     }
+    public boolean isEmpty() {
+        return customers.isEmpty();
+    }
+
+    public int size() {
+        return customers.size();
+    }
+    
+    public void enqueue(Customer customer) {
+        customers.addLast(customer);
+        System.out.println(customer.getName() + " has been added to the queue.");
+    }
 }
