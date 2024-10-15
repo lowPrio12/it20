135b65eaab8fc1add254318c6ffc3ede228ad74e
lowPrio12 <ninar.ranin@gmail.com>
2024-10-15 20:40:23 +0800

    Added Scanner to get the input of the user

--- src/topic_4_queue/Main.java
+++ src/topic_4_queue/Main.java
@@ -1,25 +1,39 @@
 package topic_4_queue;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         Queue customerQueue = new Queue();
-        
-        // Adding customers to the queue
-	     customerQueue.enqueue(new Customer("Kent Coming"));
-	     customerQueue.enqueue(new Customer("Dexter Saldivar"));
-	     customerQueue.enqueue(new Customer("Joshua Gabitano"));
-	     
-	     System.out.println();
-	     customerQueue.displayQueue();
-	     
-	     // Serve 
-	     customerQueue.dequeue();
-	     
-	     System.out.println();
-	     customerQueue.displayQueue();
 
+        System.out.println("Welcome to the Customer Queue System!");
 
+        while (true) {
+            System.out.println("\nOptions:");
+            System.out.println("1. Add Customer");
+            System.out.println("2. Serve Customer");
+            System.out.println("3. Display Queue");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+
+            String choice = sc.nextLine();
+
+            /* Adding customers to the queue
+             customerQueue.enqueue(new Customer("Kent Coming"));
+             customerQueue.enqueue(new Customer("Dexter Saldivar"));
+             customerQueue.enqueue(new Customer("Joshua Gabitano"));
+	     
+             System.out.println();
+             customerQueue.displayQueue();
+	     
+             // Serve 
+             customerQueue.dequeue();
+	     
+             System.out.println();
+             customerQueue.displayQueue();
+             */
+        }
     }
 }
