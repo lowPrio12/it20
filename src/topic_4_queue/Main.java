e9993ece3a5dcbedef3d52912abead5e83515dd8
lowPrio12 <ninar.ranin@gmail.com>
2024-10-15 20:27:08 +0800

    Implement main class to demonstrate customer queue functionality

--- src/topic_4_queue/Main.java
+++ src/topic_4_queue/Main.java
@@ -1,15 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topic_4_queue;
 
-/**
- *
- * @author Ranin
- */
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Queue customerQueue = new Queue();
+        
+        // Adding customers to the queue
+	     customerQueue.enqueue(new Customer("Cliff"));
+	     customerQueue.enqueue(new Customer("Janoi"));
+	     customerQueue.enqueue(new Customer("Redd"));
+	     
+	     System.out.println();
+	     customerQueue.displayQueue();
+	     
+	     // Serve 
+	     customerQueue.dequeue();
+	     
+	     System.out.println();
+	     customerQueue.displayQueue();
+
+
+    }
 }
