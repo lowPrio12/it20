c81611b3a6cd82deac16cd071be849c26d836745
lowPrio12 <ninar.ranin@gmail.com>
2024-11-26 20:57:49 +0800

    added Import Scanner class for user input handling and user input for array size and elements using Scanner

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -1,5 +1,7 @@
 package topic_6_sorting;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ranin
@@ -20,13 +22,30 @@ public class BubbleSort {
             }
         }
     }
+
     public static void printArray(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Original array:");
+        printArray(arr);
+        bubbleSort(arr);
         
+        System.out.println("Sorted array:");
+        printArray(arr);
+        scanner.close();
     }
 }
