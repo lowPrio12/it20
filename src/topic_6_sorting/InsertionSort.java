ac2b6c838c45b38c63972fb49598e020ddc054b8
lowPrio12 <ninar.ranin@gmail.com>
2024-11-26 21:25:30 +0800

    added user input handling for array size and elements

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -1,11 +1,14 @@
 package topic_6_sorting;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ranin
  */
 public class InsertionSort {
-     public static void insertionSort(int[] arr) {
+
+    public static void insertionSort(int[] arr) {
         int n = arr.length;
         for (int i = 1; i < n; i++) {
             int key = arr[i];
@@ -18,17 +21,24 @@ public class InsertionSort {
             arr[j + 1] = key;
         }
     }
-     
-      public static void printArray(int[] arr) {
+
+    public static void printArray(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
-      
-       public static void main(String[] args) {
-        
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
-    
-    
+    }
+
 }
