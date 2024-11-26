f513a6749b5a4d0911bc98543d99121003c196ef
lowPrio12 <ninar.ranin@gmail.com>
2024-11-26 21:21:00 +0800

    added printArray method to display array elements

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -18,6 +18,13 @@ public class InsertionSort {
             arr[j + 1] = key;
         }
     }
+     
+      public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
     
     
 }
