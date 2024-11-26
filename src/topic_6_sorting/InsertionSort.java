7f7cf1eb4be0647c95c43fb601eaf2b130190dcd
lowPrio12 <ninar.ranin@gmail.com>
2024-11-26 21:17:14 +0800

    added insertionSort method to sort the array

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topic_6_sorting;
 
 /**
@@ -11,5 +5,19 @@ package topic_6_sorting;
  * @author Ranin
  */
 public class InsertionSort {
+     public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    
     
 }
