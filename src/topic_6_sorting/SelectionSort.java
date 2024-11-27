0798edc5d0dfe6ff7e5b7dc0d77b03975e36bdcf
lowPrio12 <ninar.ranin@gmail.com>
2024-11-28 00:32:15 +0800

    Added printArray method to print array elements

--- src/topic_6_sorting/SelectionSort.java
+++ src/topic_6_sorting/SelectionSort.java
@@ -20,4 +20,10 @@ public class SelectionSort {
             arr[i] = temp;
         }
     }
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
