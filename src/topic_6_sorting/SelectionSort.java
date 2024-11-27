a03811516d5d873f4e1f613d5dc9184ae76a38d5
lowPrio12 <ninar.ranin@gmail.com>
2024-11-28 00:31:20 +0800

    added SelectionSort algorithm

--- src/topic_6_sorting/SelectionSort.java
+++ src/topic_6_sorting/SelectionSort.java
@@ -6,5 +6,18 @@ import java.util.Scanner;
  * @author Ranin
  */
 public class SelectionSort {
-    
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
