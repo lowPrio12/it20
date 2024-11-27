5917cc1821201811fe74f18de3c4b31d61004d69
lowPrio12 <ninar.ranin@gmail.com>
2024-11-27 22:47:13 +0800

    added class scanner

--- src/topic_6_sorting/MergeSort.java
+++ src/topic_6_sorting/MergeSort.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topic_6_sorting;
-
+import java.util.Scanner; // Import Scanner class
 /**
  *
  * @author Ranin
