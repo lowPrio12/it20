5aa5e574e7bb4315042371f3e3c37ee288a5d8ab
lowPrio12 <ninar.ranin@gmail.com>
2024-11-27 23:31:24 +0800

    Added printArray method to print array elements

--- src/topic_6_sorting/QuickSort.java
+++ src/topic_6_sorting/QuickSort.java
@@ -36,6 +36,12 @@ public class QuickSort {
 
         return i + 1;
     }
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
     
 }
 
