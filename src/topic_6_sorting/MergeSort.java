97627d553312537eb95736e07151197950bc0d1a
lowPrio12 <ninar.ranin@gmail.com>
2024-11-27 23:01:37 +0800

    Added printArray method to print array elements

--- src/topic_6_sorting/MergeSort.java
+++ src/topic_6_sorting/MergeSort.java
@@ -41,4 +41,12 @@ public class MergeSort {
             arr[k++] = right[j++];
         }
     }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " "); 
+        }
+        System.out.println();
+    }
+
 }
